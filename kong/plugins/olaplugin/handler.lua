f9c88e9761ec95d11136194afc08c004adcea978
Mohit Agarwal <mohitagarwalferrari@gmail.com>
2021-05-31 17:33:43 +0530

    adding schema

--- kong/plugins/olaplugin/handler.lua
+++ kong/plugins/olaplugin/handler.lua
@@ -67,7 +67,7 @@ function plugin:access(plugin_conf)
   -- your custom code here
   kong.log.debug("saying hello from the 'access' handler")
   kong.log.debug("kong.router.get_route() : " .. kong.request.get_path())
-  return kong.response.exit(plugin_conf.http_status_code, "Success Short Circuit")
+  return kong.response.exit(plugin_conf.http_status_code, "Success Short Circuit " .. kong.request.get_path())
   -- kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
   -- kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
 
